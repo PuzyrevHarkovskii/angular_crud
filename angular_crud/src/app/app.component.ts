c8d738a5a2a07f53c4b244900388bd94e337975a
Владислав <pahnetstarushkami@bk.ru>
2024-08-12 19:17:11 +0300

    open/close modal window

--- angular_crud/src/app/app.component.ts
+++ angular_crud/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,39 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular_crud';
+  @ViewChild('myModal') modal: ElementRef | undefined;
+  studentObj: Student = new Student();
+
+  openModal() {
+    const modal = document.getElementById('myModal');
+    if (modal != null) {
+      modal.style.display = 'block';
+    }
+  }
+  closeModal() {
+    if (this.modal != null) {
+      this.modal.nativeElement.style.display = 'none';
+    }
+  }
+}
+
+export class Student {
+  name: string;
+  movileNo: string;
+  email: string;
+  city: string;
+  pincode: string;
+  address: string;
+
+  constructor() {
+    this.address = '';
+    this.city = '';
+    this.email = '';
+    this.movileNo = '';
+    this.name = '';
+    this.pincode = '';
+  }
 }
